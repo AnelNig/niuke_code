4c79d828956a58b3147f56bf71aafa12ff23443a
= <ma_cheng163@163.com>
2022-07-31 17:10:10 +0800

    修改

--- src/basic_code/basic_class_01/Code_06_BucketSort.java
+++ src/basic_code/basic_class_01/Code_06_BucketSort.java
@@ -11,6 +11,7 @@ public class Code_06_BucketSort {
 	public static void bucketSort(int[] arr) {
 		if (arr == null || arr.length < 2) {
 			return;
+//			注释
 		}
 		//找到这个数组中的最大值
 		int max = Integer.MIN_VALUE;
